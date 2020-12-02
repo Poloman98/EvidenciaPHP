99b5c431fb4fa1c69175d5e07db5c65523af95f2
Sebastian Barona <ruben9828@gmail.com>
2020-12-01 23:12:07 -0500

    Subiendo evidencia PHP

---
+++ Pagina02.php
@@ -0,0 +1,89 @@
+<?php
+
+   include_once 'Libreria/Connection.php';
+   $objConnection=new Connection();
+    
+   $sql="select codigo,nombres,apellidos,edad,correo from estudiantes";    
+   $conexion=$objConnection->getConnect();    
+   $conect= mysqli_query($conexion, $sql);
+
+   if (!$conect){
+    echo mysqli_errno($conexion);
+    echo "<script type='text/javascript'>
+            alert('Error al consultar datos');
+          </script>";
+}
+else {
+    //echo "Registro Exitoso";
+    echo "<script type='text/javascript'>
+                alert('Consulta exitosa');
+          </script>";
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport" />
+        <meta content="" name="description" />
+        <meta content="" name="author" />
+
+        <link rel='shortcut icon' href='' type='image/x-icon' sizes="16x16" />
+        <!-- ================== ESTILOS CSS ================== -->
+                
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Goldman&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="shortcut icon" type="image/x-icon" href="IMGS/ima01.ico" />
+        <link href="CSS/estilo.css" rel="stylesheet"/>
+        <link href="CSS/bootstrap.min.css" rel="stylesheet"/>        
+                 
+        <title>Registro de usuarios</title>
+    </head>
+    <body class="body-primario">        
+        <div class="col-md-8 mx-auto" style="margin-top:180px;">                                           
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nombre</th>
+                        <th>Apellido</th>
+                        <th>edad</th>
+                        <th>correo</th>                            
+                    </tr>
+                </thead>
+                <tbody>                 
+                <?php
+                
+                
+                    while ($con = mysqli_fetch_row($conect))
+                    {                
+                        echo "<tr>";
+                        echo "<td align='center'>".$con[0]."</td>";
+                        echo "<td align='center'>".$con[1]."</td>";
+                        echo "<td align='center'>".$con[2]."</td>";
+                        echo "<td align='center'>".$con[3]."</td>";
+                        echo "<td align='center'>".$con[4]."</td>";                        
+                        echo "</tr>";
+                    }
+            
+                ?>                                               
+                    <!-- <tr>
+                        <td align="center"><?php //echo $_POST['id'] ?></td>
+                        <td align="center"><?php //echo $_POST['nombre'] ?></td>
+                        <td align="center"><?php //echo $_POST['apellido'] ?></td>
+                        <td align="center"><?php //echo $_POST['edad'] ?></td>
+                        <td align="center"><?php //echo $_POST['correo'] ?></td>
+                    </tr>                                               -->
+                </tbody>
+            </table>                        
+        </div>        
+        <div>
+            <center>
+                <a href="index.php" style="color:red; font-family: 'Goldman', cursive;" >Regresar a la pagina principal</a>
+            </center>
+        </div>                
+    </body>
+</html>    
---
+++ index.php
@@ -0,0 +1,108 @@
+﻿<?php
+    include_once 'Libreria/Connection.php';
+    $objConnection=new Connection();        
+?>
+
+<DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport" />
+        <meta content="" name="description" />
+        <meta content="" name="author" />
+
+        <link rel='shortcut icon' href='' type='image/x-icon' sizes="16x16" />
+        <!-- ================== ESTILOS CSS ================== -->
+                        
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Goldman&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="shortcut icon" type="image/x-icon" href="IMGS/ima01.ico" />
+        <link href="CSS/estilo.css" rel="stylesheet"/>
+        <link href="CSS/bootstrap.min.css" rel="stylesheet"/>
+     
+        <title>Registro de usuarios</title>
+    </head>
+    
+    <body class="body-primario">
+
+        <div class="content01">
+            <div class="row" >
+                <div class="col-12 col-xl-3 col-lg-4 col-md-6 col-sm-12 mx-auto">                
+                    <form name="formulario1" method="post" action="Pagina02.php" >
+                        <div class="container venreg">
+                            <h1>Registro</h1>
+                            <p>En el siguiente formulario puede agregar nuevos estudiantes al curso</p>                         
+                            <label>ID</label><br>
+                            <input type="number" placeholder="Ingrese ID" name="id" id="id" required>
+                            <label>Nombres</label><br>
+                            <input type="text" placeholder="Ingrese Nombres" name="nombre" id="nombre" required>
+                            <label>Apellidos</label><br>
+                            <input type="text" placeholder="Ingrese Apellidos" name="apellido" id="apellido" required><br>
+                            <label>Edad</label><br>
+                            <input type="number" placeholder="Ingrese Edad" name="edad" id="edad" required>
+                            <label>Correo</label><br>
+                            <input type="text" placeholder="correo" name="correo" id="correo" required>                                                        
+                            
+                            <p style="font-size: 0.8em; color : cyan;">Creado por Sebastian barona</p>
+                            <div class="clearfix" align="left">
+                                <!-- <button type="button" id="btn-envio" class="btn btn-success">Enviar</button> -->
+                                <input type="submit"class="btn btn-success" name="envio" value="Enviar"/>
+                                <button type="button" class="btn btn-danger" onclick="limpiar()">Limpiar</button>                                
+                            </div>
+                        </div>
+                    </form>
+                </div>                            
+            </div>            
+        </div>                                      
+    </body>
+
+    <script type="text/javascript">
+            
+        function limpiar()
+        {                    
+                document.getElementById("id").value = "";
+                document.getElementById("nombre").value = "";
+                document.getElementById("apellido").value = "";
+                document.getElementById("edad").value = "";
+                document.getElementById("correo").value = "";
+        }                
+            
+    </script>
+</html>
+
+<?php
+    
+    if(!empty($_POST['envio']))     
+    {    
+        settype($id,"integer");
+        settype($nombre,"string");
+        settype($apellido,"string");
+        settype($edad,"integer");
+        settype($correo,"string");
+        
+        $id       = $_POST['id'];
+        $nombre   = $_POST['nombre'];
+        $apellido = $_POST['apellido'];
+        $edad     = $_POST['edad'];
+        $correo   = $_POST['correo'];
+    
+        $sql="insert into estudiantes values ($id,'$nombre','$apellido',$edad,'$correo')";    
+        $conexion=$objConnection->getConnect();        
+        $ejecucion=mysqli_query($conexion, $sql);
+        
+        if (!$ejecucion){
+            echo mysqli_errno($conexion);
+            echo "<script type='text/javascript'>
+                    alert('Error al insertar datos');
+                  </script>";
+        }
+        else {
+            //echo "Registro Exitoso";
+            echo "<script type='text/javascript'>
+                        alert('Registro Exitoso');
+                  </script>";
+        }
+    }        
+
+?>
